37d6eb72a143ab708950ca33db12e131ce56bfa2
atilarangel <superatilarangel@gmail.com>
2018-06-22 15:27:06 -0300

    fix bugs

--- index.js
+++ index.js
@@ -1,40 +1,55 @@
-var canvas = document.getElementById('canvas')
-var opts = {
+const canvas = document.getElementById('canvas')
+const opts = {
     errorCorrectionLevel: 'H',
     type: 'svg',
-    scale: 10,
+    scale: 5,
     rendererOpts: {
-      quality: 0.3,
-      scale: 10,
+      quality: 0.3
     }
 }
-const text = document.getElementById("myTextarea")
-const filename = document.getElementById("filename").value
-console.log(url)
-text.style.display = 'none';
+
+let svg
 
 qrCode=()=>{
-    const url = document.getElementById("url").value
-    QRCode.toCanvas(canvas, url,opts, function (error) {
-    if (error) console.error(error)
-    console.log('success!');
-    })
+    //https://www.npmjs.com/package/qrcode
 
-    QRCode.toString(canvas, url, opts, function (err, string) {
-        if (err) throw err
-        text.value = string;
-})
+    const url = document.getElementById("url").value 
+
+    console.log()
+    if (url.length>=1) {
+        QRCode.toCanvas(
+        canvas,
+        url,
+        opts,
+        function(error) {
+            if (error)
+            console.error(error);
+            console.log("success!");
+        })
+
+        QRCode.toString(
+        canvas,
+        url,
+        opts,
+        function(err, string) {
+            if (err) throw err;
+            svg = string;
+        })
+    }
 }
-download=(filename)=>{
-    var element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text.value));
-    console.log(filename)
-    element.setAttribute('download', filename + '.svg');
+download=()=>{
+    //https://stackoverflow.com/questions/3665115/create-a-file-in-memory-for-user-to-download-not-through-server
+    const filename = document.getElementById("filename").value;
+    var file = document.createElement('a')
+
+    file.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(svg))
+    file.setAttribute('download', filename + '.svg')
   
-    element.style.display = 'none';
-    document.body.appendChild(element);
+    file.style.display = 'none'
+    document.body.appendChild(file);
   
-    element.click();
+    file.click();
   
-    document.body.removeChild(element);
+    document.body.removeChild(file);
 }
+
